0f0e63effe63c9466c02b898514e8a6cb22edbc5
Kevin Jump <kevin@thejumps.co.uk>
2018-06-23 11:47:50 +0100

    Add the PropertyValue converter for Styled.Textbox

---
+++ Our.Umbraco.StyledEditors/Converters/StyledTextboxPropertyConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.StyledEditors.Converters
+{
+    [PropertyValueType(typeof(string))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class StyledTextboxPropertyConverter : PropertyValueConverterBase
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.InvariantEquals("Styled.Textbox");
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source ?? string.Empty;
+        }
+
+        public override object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+    }
+}
